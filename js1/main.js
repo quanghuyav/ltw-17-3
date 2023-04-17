00d547b1adc63cea5be7d2e7dd7bd15e61b535d1
quanghuyav <huyav1999@gmail.com>
2023-04-17 21:36:54 +0700

    z

---
+++ js1/main.js
@@ -0,0 +1,26 @@
+// Javascript là ngôn ngữ lập trình
+// Javascript không phải là Java, không liên quan đến Java
+// Javascript làm frontend
+
+
+// CORE 
+// Biến, hằng, quy tắc đặt tên, toán tử, kiểu dữ liệu, if else, for while do while, hàm, obj...
+
+
+// DOM: 
+// ẩn hiện phần tử, tương tác giao diện
+// Lấy dữ liệu
+// Tương tác dữ liệu
+// Đẩy ra giao diện
+// event
+
+
+// Nâng cao 
+// Xử lí logic cơ bản
+// Lấy api, các thư viện
+// callback, xử lí bất đồng hộ
+// ES6
+
+// REACTJS
+// 1+2+4+5+6+"7" 187  1+2+"3"+4+5 3345
+
